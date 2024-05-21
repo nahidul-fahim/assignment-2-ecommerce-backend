9ba400ee5a6c01034fb31772e6324bac817fb49b
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-21 23:29:04 +0600

    created interface for product

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 import app from "./app"
 import config from "./app/config";
-
-const port = 5000;
-
 
 async function main() {
     try {
         await mongoose.connect(config.db_url as string);
         app.listen(config.port, () => {
-            console.log(`eCommerce backend listening on port ${config.port}`)
+            console.log(`eCommerce backend is running on port ${config.port}`)
         })
     } catch (error) {
         console.log("Error from main:", error);
     }
 }
 
-
-
-
+main();
