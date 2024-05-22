6f468e92aa493ff481b9cc6d995d67c016c3c207
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-22 07:45:15 +0600

    created model for products

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -1,1 +1,71 @@
 import { Schema, model } from 'mongoose';
+import { TInventory, TProduct, TVariants } from './product.interface';
+
+// variants schema
+const variantsSchema = new Schema<TVariants>({
+    type: {
+        type: String,
+        required: [true, 'Type is required'],
+        trim: true,
+    },
+    value: {
+        type: String,
+        required: [true, 'Value is required'],
+        trim: true,
+    },
+})
+
+
+// inventory schema
+const inventorySchema = new Schema<TInventory>({
+    quantity: {
+        type: Number,
+        required: [true, 'Quantity is required'],
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, 'In stock is required'],
+    },
+})
+
+
+// product schema
+const productSchema = new Schema<TProduct>({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        trim: true,
+        minLength: [20, 'Min length is 20. But got {VALUE}']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        trim: true,
+        min: [0, 'Min price is 0. But got {VALUE}']
+    },
+    category: {
+        type: String,
+        required: [true, 'Category is required'],
+        trim: true,
+    },
+    tags: {
+        type: [String],
+        required: [true, 'Tags are required'],
+    },
+    variants: {
+        type: [variantsSchema],
+        required: [true, 'Variants are required'],
+    },
+    inventory: {
+        type: inventorySchema,
+        required: [true, 'Inventory is required'],
+    },
+});
+
+
+export const Product = model<TProduct>('Product', productSchema);
